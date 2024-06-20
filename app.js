e1c3b833ece594b186871d5d2519ad2cf391678f
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-20 11:58:51 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write code here
+let a = undefined;
+let b = 2;
+let c = a++;
+
+let d = String(a)
+let e = String(b)
+let f = String(c)
+console.log(d, e, f)
+//d = `NaN`, e = `2`, f = `NaN`
+//เพราะว่า a เป็น undefined ตั้งแต่แรกอยู่แล้ว ทำให้ c เป็น undefined ไปด้วย เพราะ a เป็น NaN เมื่อ increment แล้วก็ยังเป็น NaN อยู่ดี
